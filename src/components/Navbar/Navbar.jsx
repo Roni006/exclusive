c32e913a544238a747b3b403dbf0038331bb6181
Anik Islam <anik54908@gmail.com>
2025-11-24 12:55:03 +0600

    initialized Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className='bg-[#000000]'>
+                <div className="container">
+                    <div className="flex items-center py-3">
+                        <div className=" w-[80%] text-end">
+                            <p className="text-[#ffffff] text-[14px] font-poppins leading-[21px]flex ">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! 
+                                <a href="#" className="font-semibold leading-6 underline">Shop Now</a></p>
+                        </div>
+                        <div className=" w-[20%] text-end">
+                            <p className="text-white">english</p>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
