3c7ef25654d0741cb6a0199e9ea7fca7ef5e9f64
Anik Islam <anik54908@gmail.com>
2025-12-04 11:01:25 +0600

    input resolve

--- src/components/BIllingDetails/BIllingDetails.jsx
+++ src/components/BIllingDetails/BIllingDetails.jsx
@@ -74,64 +74,18 @@ const BIllingDetails = () => {
                                 </div>
 
                                 <div>
-                                    <div className="flex items-center gap-4 ">
-                                        {/* <input className="cursor-pointer border" type="radio" id='stripe' name="dot" />
-                                        <label className="cursor-pointer" htmlFor="stripe">Stripe</label> */}
-                                        <input
-                                            id="radio"
-                                            type="radio"
-                                            name="value1"
-                                            className="w-5 h-5 hidden peer"
-                                            defaultChecked=""
-                                        />
-                                        <label
-                                            htmlFor="radio"
-                                            className="relative flex items-center justify-center p-1 peer-checked:before:hidden before:block before:absolute before:w-full before:h-full before:bg-white w-6 h-6 cursor-pointer border-2 border-black rounded-full overflow-hidden"
-                                        >
-                                            <span className="bg-black rounded-full w-3 h-3" />
-                                        </label>
-                                        <label htmlFor="radio">Stripe</label>
-
-
+                                    
+                                    <div className="flex items-center gap-4 mb-4">
+                                       <input type="radio" id="fs" name="value1" className="accent-black w-5 h-5"/>
+                                       <label htmlFor="fs" className="cursor-pointer text-[16px] font-poppins leading-6 font-normal">Strip</label>
                                     </div>
-                                    <div className="flex items-center gap-4 ">
-                                          <input
-                                            id="cash"
-                                            type="radio"
-                                            name="value1"
-                                            className="w-5 h-5 hidden peer"
-                                            defaultChecked=""
-                                        />
-                                        <label
-                                            htmlFor="cash"
-                                            className="relative flex items-center justify-center p-1 peer-checked:before:hidden before:block before:absolute before:w-full before:h-full before:bg-white w-6 h-6 cursor-pointer border-2 border-black rounded-full overflow-hidden"
-                                        >
-                                            <span className="bg-black rounded-full w-3 h-3" />
-                                        </label>
-                                        <label htmlFor="cash">Stripe</label>
+                                    
+                                    <div className="flex items-center gap-4 mb-4">
+                                       <input type="radio" id="fss" name="value1" className="accent-black w-5 h-5"/>
+                                       <label htmlFor="fss" className="cursor-pointer text-[16px] font-poppins leading-6 font-normal">Cash On Delivey</label>
                                     </div>
-                                </div>
-
-                                {/* <div className="space-x-6 flex">
-                                    <div className="flex items-center">
-                                        <input
-                                            id="radio"
-                                            type="radio"
-                                            name="value1"
-                                            className="w-5 h-5 hidden peer"
-                                            defaultChecked=""
-                                        />
-                                        <label
-                                            htmlFor="radio"
-                                            className="relative flex items-center justify-center p-1 peer-checked:before:hidden before:block before:absolute before:w-full before:h-full before:bg-white w-6 h-6 cursor-pointer border-2 border-black rounded-full overflow-hidden"
-                                        >
-                                            <span className="bg-black rounded-full w-3 h-3" />
-                                        </label>
-                                    </div>
-                                </div> */}
-
-
-
+                               
+                                </div> 
 
                             </div>
                             <div className="flex justify-between items-center mt-8">
