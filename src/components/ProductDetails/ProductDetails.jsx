5c13d5ace890aeac9711d83285d99ec79030fd00
Anik Islam <anik54908@gmail.com>
2025-12-15 00:27:56 +0600

    almost complete product details section

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -9,7 +9,7 @@ import keyboard from "../../assets/images/product/keyboard.png";
 import monitor from "../../assets/images/product/monitor.png";
 import cooler from '../../assets/sellingproduct/cooler.png'
 import { FaStar } from "react-icons/fa";
-import { TiStarHalfOutline } from "react-icons/ti";
+// import { TiStarHalfOutline, TiTabsOutline } from "react-icons/ti";
 import { TiStarFullOutline } from "react-icons/ti";
 const ProductDetails = () => {
     const gallery = [
@@ -52,15 +52,18 @@ const ProductDetails = () => {
             disPrice: 160,
             price: 170,
             image: cooler,
-            rating: 65, 
+            rating: 65,
         },
     ]
-    const [previewImg, setPreviewImg] = useState(preview)
+    const [previewImg, setPreviewImg] = useState(preview);
+
+    const rating = 4;
+ 
     return (
 
         < section className='pt-[110px] pb-15' >
             <div className="container">
-                <div className="flex">
+                <div className="flex   gap-[70px] ">
                     <div className="w-[60%]">
                         <div className='flex gap-[30px]'>
                             <div className="w-[30%] flex flex-col gap-4 items-center">
@@ -118,36 +121,69 @@ const ProductDetails = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='w-[40%]'></div>
+                    <div className='w-[40%]'>
+                        <h2 className="text-[24px] font-inter font-semibold text-[#000000] leading-6 tracking-[3%] mb-4">Havic HV G-92 Gamepad</h2>
+
+                        <div className='flex items-center'>
+
+                            {
+                                Array.from({ length: 5 }).map((_, i) => (
+                                    <TiStarFullOutline
+                                        key={i}
+                                        className={i < rating ? "text-[#FFAD33] text-[20px] " : "text-gray-300 text-[20px]"}
+                                    />
+                                ))
+                            }
+                            <span className='ml-2 text-[rgba(16,3,3,0.5)] font-poppins font-normal leading-3.5 '>(150 Reviews)</span>
+
+                        </div>
+                        <h3 className='font-inter font-normal leading-6 text-[#000000] tracking-[%3] text-[24px] pt-4 pb-6'> $192.00</h3>
+                        <p className='font-poppins font-normal text-[#000000] leading-[21px] text-[14px] w-[373px] border-b-2 border-[rgba(0,0,0,0.5)] pb-6
+                        '>PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.</p>
+
+                        <div className='color pt-6 flex items-center gap-6'>
+                            <div>
+                                <p className=' items-center  text-[20px] leading-5 text-[#000000] tracking-[3%] font-poppins'>Colors: </p>
+                            </div>
+                            <div>
+                                <input type="radio" name='radio' id='color1' className='hidden' />
+                                <label htmlFor="color1" className=' cursor-pointer '>
+                                    <span className='w-5 h-5 inline-block rounded-full bg-[#A0BCE0] '></span>
+                                </label>
+                            </div>
+                        </div>
+
+                   
+                    </div>
                 </div>
 
                 <div className="grid grid-cols-4 gap-[30px] items-center justify-between mt-15 ">
                     {
                         reletedProdcuts.map(related => (
-                         <div className="product-item">
-                        <div className=" bg-[#F5F5F5] py-[35px] px-10 rounded-sm relative">
-                            <img src={related.image} alt="" />
-                            <div className="discount bg-[#DB4444] w-[55px] h-[26px] text-center rounded-sm absolute top-3 left-3">
-                                <span className="text-[12px] text-[#FAFAFA] ">-{related.discount}%</span>
+                            <div className="product-item">
+                                <div className=" bg-[#F5F5F5] py-[35px] px-10 rounded-sm relative">
+                                    <img src={related.image} alt="" />
+                                    <div className="discount bg-[#DB4444] w-[55px] h-[26px] text-center rounded-sm absolute top-3 left-3">
+                                        <span className="text-[12px] text-[#FAFAFA] ">-{related.discount}%</span>
+                                    </div>
+                                </div>
+                                <div className="details pt-4">
+                                    <h2 className="text-[18px] leading-6 font-poppins text-[#000000] font-medium">{related.name}</h2>
+                                    <p className="text-[16px] leading-6 font-poppins font-semibold flex gap-3 py-2" >
+                                        <span className=" text-[#DB4444]" >${related.disPrice}</span>
+                                        <del className="text-[rgba(0,0,0,0.5)]"> ${related.price}</del>
+                                    </p>
+                                    <div className="raring flex gap-1">
+                                        <TiStarFullOutline className="text-[#FFAD33] text-[20px] " />
+                                        <TiStarFullOutline className="text-[#FFAD33] text-[20px] " />
+                                        <TiStarFullOutline className="text-[#FFAD33] text-[20px] " />
+                                        <TiStarFullOutline className="text-[#FFAD33] text-[20px] " />
+                                        <TiStarFullOutline className="text-[#FFAD33] text-[20px] " />
+                                        <span className="text-[14px] leading-[21px] font-poppins font-semibold text-[rgba(0,0,0,0.5)] ">({related.rating})</span>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="details pt-4">
-                            <h2 className="text-[18px] leading-6 font-poppins text-[#000000] font-medium">{related.name}</h2>
-                            <p className="text-[16px] leading-6 font-poppins font-semibold flex gap-3 py-2" >
-                                <span className=" text-[#DB4444]" >${related.disPrice}</span>
-                                <del className="text-[rgba(0,0,0,0.5)]"> ${related.price}</del>
-                            </p>
-                            <div className="raring flex gap-1">
-                                <TiStarFullOutline className="text-[#FFAD33] text-[20px] " />
-                                <TiStarFullOutline className="text-[#FFAD33] text-[20px] " />
-                                <TiStarFullOutline className="text-[#FFAD33] text-[20px] " />
-                                <TiStarFullOutline className="text-[#FFAD33] text-[20px] " />
-                                <TiStarFullOutline className="text-[#FFAD33] text-[20px] " />
-                                <span className="text-[14px] leading-[21px] font-poppins font-semibold text-[rgba(0,0,0,0.5)] ">({related.rating})</span>
-                            </div>
-                        </div>
-                    </div>
-                 ))
+                        ))
                     }
                 </div>
 
