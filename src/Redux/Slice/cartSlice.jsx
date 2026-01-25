0e159b1522c8ee79eee59cdb8f1913a5b7620739
Anik Islam <anik54908@gmail.com>
2026-01-25 14:17:56 +0600

    added cart page functionality

--- src/Redux/Slice/cartSlice.jsx
+++ src/Redux/Slice/cartSlice.jsx
@@ -1,28 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    items: JSON.parse(localStorage.getItem('cart')) || []
-}
+  items: JSON.parse(localStorage.getItem("cart")) || [],
+};
 
-const cartSlice  = createSlice({
-    name:'cart',
-    initialState,
-    reducers: {
-        addToCart:(state, action) => {
-            const item = action.payload
-            const existing = state.items.find(i => i.id === item.id)
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existing = state.items.find((i) => i.id === item.id);
 
-            if(existing){
-                existing.quantity += item.quantity
-            }
-            else{
-                state.items.push(item)
-            }
-            localStorage.setItem('cart', JSON.stringify(state.items))
-        }, 
-    }
-})
+      if (existing) {
+        existing.quantity += item.quantity;
+      } else {
+        state.items.push(item);
+      }
+      localStorage.setItem("cart", JSON.stringify(state.items));
+    },
+    updateQuantity: (state, action) => {
+      const { id, quantity } = action.payload;
+      const product = state.items.find((i) => i.i === id);
 
-export const { addToCart } = cartSlice.actions
+      if (product) {
+        product.quantity = quantity;
+      }
+      localStorage.setItem("cart", json.stringify(state.items));
+    },
+  },
+});
 
-export default cartSlice.reducer
+export const { addToCart, updateQuantity } = cartSlice.actions;
+
+export default cartSlice.reducer;
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,138 +1,174 @@
 import monitor from "../../assets/cart/monitor.png";
-import console from "../../assets/cart/concole.png"
+import consoles from "../../assets/cart/concole.png";
 import arrowUp from "../../assets/cart/arrowUp.svg";
-import arrowDown from "../../assets/cart/arrpwDown.svg"
+import arrowDown from "../../assets/cart/arrpwDown.svg";
 import { useState } from "react";
 import { Link } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { updateQuantity } from "../../Redux/Slice/cartSlice";
+import { MdKeyboardArrowUp } from "react-icons/md";
+import { MdKeyboardArrowDown } from "react-icons/md";
 const Cart = () => {
-    const [price, setPrice] = useState({
-        price1: 650,
-        price2: 550
-    })
-    const [counts, setCounts] = useState({
-        button1: 1,
-        button2: 1,
-    })
-    const handleIncrement = (button) => {
-        setCounts((prevCounts) => ({
-            ...prevCounts,
-            [button]: prevCounts[button] + 1
-        }))
+  const [counts, setCounts] = useState({
+    button1: 1,
+    button2: 1,
+  });
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart.items);
+
+  const incrementQuantity = (item) => {
+    dispatch(
+      updateQuantity({
+        id: item.id,
+        quantity: item.quantity + 1,
+      }),
+    );
+  };
+  const deccrementQuantity = (item) => {
+    if (item.quantity > 1) {
+      dispatch(
+        updateQuantity({
+          id: item.id,
+          quantity: item.quantity - 1,
+        }),
+      );
     }
-    const handleDecrement = (button) => {
-        if (counts.button1 > 1 || counts.button2 > 1) {
-            setCounts((prevCounts) => ({
-                ...prevCounts,
-                [button]: prevCounts[button] - 1
-            }))
-        }
-    }
+  };
+  return (
+    <section className="pt-40 pb-28 ">
+      <div className="container">
+        <div className="ctitle pb-[50px]">
+          <h1 className="text-[20px] font-poppins leading-6 text-[#000000]">
+            Cart
+          </h1>
+        </div>
+        {/*todo:  not working => `shadow-(--shadow-cart)` */}
+        <div className="titles grid grid-cols-4 py-6 px-10 shadow-md rounded-4 mb-10 ">
+          <h2 className="text-[16px] text-[#000000] leading-6 font-poppins ">
+            Product
+          </h2>
+          <h2 className="text-[16px] text-[#000000] leading-6 font-poppins text-center">
+            Price
+          </h2>
+          <h2 className="text-[16px] text-[#000000] leading-6 font-poppins text-center">
+            Quantity
+          </h2>
+          <h2 className="text-[16px] text-[#000000] leading-6 font-poppins text-end">
+            Subtotal
+          </h2>
+        </div>
 
-    const totalAmoutn = (price.price1*counts.button1) + (price.price2*counts.button2)
-
-    return (
-        <section className='pt-40 pb-28 '>
-            <div className="container">
-                <div className='ctitle pb-[50px]'>
-                    <h1 className='text-[20px] font-poppins leading-6 text-[#000000]'>Cart</h1>
-                </div>
-                {/*todo:  not working => `shadow-(--shadow-cart)` */}
-                <div className="titles grid grid-cols-4 py-6 px-10 shadow-md rounded-4 mb-10 ">
-                    <h2 className='text-[16px] text-[#000000] leading-6 font-poppins '>Product</h2>
-                    <h2 className='text-[16px] text-[#000000] leading-6 font-poppins text-center'>Price</h2>
-                    <h2 className='text-[16px] text-[#000000] leading-6 font-poppins text-center'>Quantity</h2>
-                    <h2 className='text-[16px] text-[#000000] leading-6 font-poppins text-end'>Subtotla</h2>
-                </div>
-
-                <div className="titles grid grid-cols-4  justify-between py-6 px-10 shadow-md rounded-4 mb-10 ">
-
-                    <div className="flex items-center gap-5">
-                        <img src={monitor} alt="" />
-                        <h2 className='text-[16px] text-[#000000] leading-6 font-poppins '>LCD Monitor</h2>
-                    </div>
-
-                    <div className="flex  items-center gap-5 justify-center">
-                        <h2 className='text-[16px] text-[#000000] leading-6 font-poppins  '>$ <span>{price.price1}</span> </h2>
-                    </div>
-
-                    <div className="flex items-center gap-5 justify-center">
-                        <div className="grid grid-cols-2 items-center gap-4 w-[72px] border border-[rgba(0,0,0,0.4)] rounded-sm py-1.5 px-3">
-                            <div>
-                                <span className="text-[16px] text-[#000000] leading-6 font-poppins">{counts.button1}</span>
-                            </div>
-                            <div className="flex flex-col gap-1.5 cursor-pointer">
-                                <img onClick={() => handleIncrement('button1')} src={arrowUp} alt="Up arrow " />
-                                <img onClick={() => handleDecrement('button1')} src={arrowDown} alt="Down arrow " />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex  items-center gap-5 justify-end">
-                        <h2 className='text-[16px] text-[#000000] leading-6 font-poppins '>$<span>{price.price1 * counts.button1}</span> </h2>
-                    </div>
-                </div>
-
-                <div className="titles grid grid-cols-4  justify-between py-6 px-10 shadow-md rounded-4 mb-10 ">
-
-                    <div className="flex items-center gap-5">
-                        <img src={console} alt="" />
-                        <h2 className='text-[16px] text-[#000000] leading-6 font-poppins '>LCD Monitor</h2>
-                    </div>
-
-                    <div className="flex  items-center gap-5 justify-center">
-                        <h2 className='text-[16px] text-[#000000] leading-6 font-poppins  '>$ <span>{price.price2}</span> </h2>
-                    </div>
-
-                    <div className="flex items-center gap-5 justify-center">
-                        <div className="grid grid-cols-2 items-center gap-4 w-[72px] border border-[rgba(0,0,0,0.4)] rounded-sm py-1.5 px-3">
-                            <div>
-                                <span className="text-[16px] text-[#000000] leading-6 font-poppins">{counts.button2}</span>
-                            </div>
-                            <div className="flex flex-col gap-1.5 cursor-pointer">
-                                <img onClick={() => handleIncrement('button2')} src={arrowUp} alt="Up arrow " />
-                                <img onClick={() => handleDecrement('button2')} src={arrowDown} alt="Down arrow " />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex  items-center gap-5 justify-end">
-                        <h2 className='text-[16px] text-[#000000] leading-6 font-poppins '>$<span>{price.price2 * counts.button2}</span> </h2>
-                    </div>
-                </div>
-                <div className="flex justify-between">
-                    <a href="#" className="text-[16px] leading-6 text-[#000000] font-poppins py-4 px-12 font-medium border rounded-sm hover:bg-[#DB4444] duration-300 hover:text-white hover:border-[#DB4444] ">Return To Shop</a>
-                    <a href="#" className="text-[16px] leading-6 text-[#000000] font-poppins py-4 px-12 font-medium border rounded-sm hover:bg-[#DB4444] duration-300 hover:text-white hover:border-[#DB4444] ">Update Cart</a>
-                </div>
-
-                <div className="flex justify-end pt-28">
-                    <div className="w-[470px] border-2 py-8 px-6 rounded-md ">
-                        <h2 className="text-[20px] leadgin6
-                     text-[rgba(0,0,0)] font-medium pb-6">Cart Total</h2>
-
-                        <div className="flex justify-between border-b-2 border-[rgba(0,0,0,0.5)] pb-4 ">
-                            <h3 className="text-[16px] leading-6 text-[#000000] font-poppins font-normal">Subtoal:</h3>
-                            <span className="text-[16px] leading-6 text-[#000000] font-poppins font-normal">${totalAmoutn}</span>
-                        </div>
-
-                        <div className="flex justify-between border-b-2 border-[rgba(0,0,0,0.5)] pb-4 pt-4">
-                            <h3 className="text-[16px] leading-6 text-[#000000] font-poppins font-normal">Shipping:</h3>
-                            <span className="text-[16px] leading-6 text-[#000000] font-poppins font-normal">Free</span>
-                        </div>
-
-                        <div className="flex justify-between pb-4 pt-4">
-                            <h3 className="text-[16px] leading-6 text-[#000000] font-poppins font-normal">Total:</h3>
-                            <span className="text-[16px] leading-6 text-[#000000] font-poppins font-normal">${totalAmoutn}</span>
-                        </div>
-                        
-                        <div className="text-center pt-4">
-                            <Link to='/checkout' className="gblbdtn">Procees to checkout</Link>
-                        </div>
-                    </div>
-                </div>
-
+        {/* Product 1 */}
+        {cartItems.map((cart) => (
+          <div className="titles grid grid-cols-4  justify-between py-6 px-10 shadow-md rounded-4 mb-10 ">
+            <div className="flex items-center gap-5">
+              <img className="w-15" src={cart.image} alt="" />
+              <h2 className="text-[16px] text-[#000000] leading-6 font-poppins ">
+                {cart.title}
+              </h2>
             </div>
-        </section>
-    )
-}
 
-export default Cart
+            <div className="flex  items-center gap-5 justify-center">
+              <h2 className="text-[16px] text-[#000000] leading-6 font-poppins  ">
+                $ <span>{cart.price}</span>{" "}
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-5 justify-center">
+              <div className="grid grid-cols-2 items-center gap-4 w-[72px] border border-[rgba(0,0,0,0.4)] rounded-sm py-1.5 px-3">
+                <div>
+                  <span className="text-[16px] text-[#000000] leading-6 font-poppins">
+                    {cart.quantity}
+                  </span>
+                </div>
+                <div className="flex flex-col gap-1.5 cursor-pointer">
+                  {/* <img
+                    onClick={() => incrementQuantity(cart)}
+                    src={arrowUp}
+                    alt="Up arrow "
+                  /> */}
+                  <MdKeyboardArrowUp onClick={() => incrementQuantity(cart)} />
+                  <MdKeyboardArrowDown
+                    onClick={() => deccrementQuantity(cart)}
+                  />
+                  {/* <img
+                    onClick={() => decrementQuantity(cart)}
+                    src={arrowDown}
+                    alt="Down arrow "
+                  /> */}
+                </div>
+              </div>
+            </div>
+
+            <div className="flex  items-center gap-5 justify-end">
+              <h2 className="text-[16px] text-[#000000] leading-6 font-poppins ">
+                $<span>{cart.quantity == 1 ? cart.price * cart.quantity: Math.round(cart.price * cart.quantity)}</span>
+              </h2>
+            </div>
+          </div>
+        ))}
+
+        <div className="flex justify-between">
+          <a
+            href="#"
+            className="text-[16px] leading-6 text-[#000000] font-poppins py-4 px-12 font-medium border rounded-sm hover:bg-[#DB4444] duration-300 hover:text-white hover:border-[#DB4444] "
+          >
+            Return To Shop
+          </a>
+          <a
+            href="#"
+            className="text-[16px] leading-6 text-[#000000] font-poppins py-4 px-12 font-medium border rounded-sm hover:bg-[#DB4444] duration-300 hover:text-white hover:border-[#DB4444] "
+          >
+            Update Cart
+          </a>
+        </div>
+
+        <div className="flex justify-end pt-28">
+          <div className="w-[470px] border-2 py-8 px-6 rounded-md ">
+            <h2
+              className="text-[20px] leadgin6
+                     text-[rgba(0,0,0)] font-medium pb-6"
+            >
+              Cart Total
+            </h2>
+
+            <div className="flex justify-between border-b-2 border-[rgba(0,0,0,0.5)] pb-4 ">
+              <h3 className="text-[16px] leading-6 text-[#000000] font-poppins font-normal">
+                Subtotal:
+              </h3>
+              <span className="text-[16px] leading-6 text-[#000000] font-poppins font-normal">
+                $100
+              </span>
+            </div>
+
+            <div className="flex justify-between border-b-2 border-[rgba(0,0,0,0.5)] pb-4 pt-4">
+              <h3 className="text-[16px] leading-6 text-[#000000] font-poppins font-normal">
+                Shipping:
+              </h3>
+              <span className="text-[16px] leading-6 text-[#000000] font-poppins font-normal">
+                Free
+              </span>
+            </div>
+
+            <div className="flex justify-between pb-4 pt-4">
+              <h3 className="text-[16px] leading-6 text-[#000000] font-poppins font-normal">
+                Total:
+              </h3>
+              <span className="text-[16px] leading-6 text-[#000000] font-poppins font-normal">
+                $100
+              </span>
+            </div>
+
+            <div className="text-center pt-4">
+              <Link to="/checkout" className="gblbdtn">
+                Procees to checkout
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cart;
